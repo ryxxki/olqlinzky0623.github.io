343b7ff40a52717088f166d16ccc5a3f46f3a1ca
olqlinzky <58416474+olqlinzky@users.noreply.github.com>
2020-01-12 12:27:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 const menu_btn = document.querySelector(".menu-btn");
-const nav = document.querySelector("ul.link");
+const nav_ul = document.querySelector("ul.link");
+const nav = document.querySelector("nav");
+const menu = document.querySelector("nav h1");
 let pilihan = false;
 
 menu_btn.addEventListener("click", () => {
 
         if(!pilihan){
             menu_btn.classList.add("active");
-            nav.classList.add("active");
+            nav_ul.classList.add("active");
             pilihan = true;
         }else{
             menu_btn.classList.remove("active");
-            nav.classList.remove("active");
+            nav_ul.classList.remove("active");
             pilihan = false;
         }
     
 });
+
+window.addEventListener("scroll", () => {
+    let test = window.pageYOffset;
+    if(test > 50 ){
+        nav.style.height = "70px";
+        navH.style.fontSize = "20px";
+    }else{
+        nav.style.height = "100px";
+        navH.style.fontSize = "30px";
+    }
+});
+
 
