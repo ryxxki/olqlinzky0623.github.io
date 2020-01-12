a38229e69b217f9753aaf88ab4a4a7e0c8752106
olqlinzky <58416474+olqlinzky@users.noreply.github.com>
2020-01-12 20:31:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menu_btn = document.querySelector(".menu-btn");
 const nav_ul = document.querySelector("ul.link");
 const nav = document.querySelector("nav");
 const navH = document.querySelector("nav h1");
+let img  = document.querySelectorAll(".img");
 let pilihan = false;
 
 menu_btn.addEventListener("click", () => {
@@ -23,10 +24,17 @@ window.addEventListener("scroll", () => {
     if(test > 50 ){
         nav.style.height = "70px";
         navH.style.fontSize = "20px";
+        menu_btn.style.top = "10px";
+        nav.style.borderBottom = "0.4px #1188FF solid";
     }else{
+        menu_btn.style.top = "25px";
         nav.style.height = "100px";
         navH.style.fontSize = "30px";
+        nav.style.borderBottom = "none";
+    }
+
+    if(test > 300){
+        
     }
 });
-
 
